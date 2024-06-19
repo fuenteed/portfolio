74fbdec951f9cf76601d79c2bca9947289cf4136
fuenteed <114704603+fuenteed@users.noreply.github.com>
2024-06-18 23:22:38 -0700

    First Commit

---
+++ my-portfolio/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from './components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h2 className="text-2xl font-bold mb-4">Welcome Edson </h2>
+      <p>This is where you can showcase your projects and skills.</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
